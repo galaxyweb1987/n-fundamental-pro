1525f73ab360558672ad315590b1e124f5424a21
galaxyweb1987 <galaxyweb1987@gmail.com>
2024-08-17 15:42:58 +0100

    feat: add songs services

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
 export class SongsController {
+  constructor(private songsService: SongsService) {}
+  @Post()
+  create() {
+    return this.songsService.create('Animals by Martin Garrix');
+  }
+
   @Get()
   findAll() {
-    return 'find all songs';
+    return this.songsService.findAll();
+  }
+  @Get(':id')
+  findOne() {
+    return 'find song based on id';
+  }
+
+  @Put(':id')
+  update() {
+    return 'update id';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'delete id';
   }
 }
